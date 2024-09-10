4a9cf8197d1ab440b5a2bbd0dc29cc30c7f6ed13
Brandon Holman <112407387+bholmdev@users.noreply.github.com>
2024-09-10 17:37:08 -0500

    main loop complete

--- pig.py
+++ pig.py
@@ -13,10 +13,8 @@ def start():
         else:
             print("Invalid, try again.")
 
-        game()
-
 num_players = start()
-max_score = 50
+max_score = 10
 player_scores = [0 for _ in range(num_players)]
 
 def roll():
@@ -26,22 +24,24 @@ def roll():
 
     return roll
     
+    
 def game():
     while max(player_scores) < max_score:
         for player in range(num_players):
-            print("\nPlayer", player + 1 + "'s turn!\n")
+            print(f"\nPlayer {player + 1}'s turn!\n")
             current_score = 0
 
             while True:
                 should_roll = input("Would you like to roll?  (y/n)  ")
 
-                if should_roll.lower() != "y" or should_roll.lower() != "yes":
+                if should_roll.lower() != "y" and should_roll.lower() != "yes":
                     break
                 
                 value = roll()
 
                 if value == 1:
                     print("You rolled a 1! Next player start")
+                    current_score = 0
                     break
                 else:
                     current_score += value
@@ -49,5 +49,7 @@ def game():
                 
                 print("Your score is:", current_score)
 
-        player_scores[player] += current_score
-        print("Your total score is:", player_scores[player])
+            player_scores[player] += current_score
+            print("Your total score is:", player_scores[player])
+    
+game()
